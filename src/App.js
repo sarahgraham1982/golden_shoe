9700110a5786441c13b74c46caf0e21458008924
sarahgraham1982 <79756187+sarahgraham1982@users.noreply.github.com>
2021-10-16 14:29:08 +0100

    added shop component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Shop from './shop.js';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <h1 className="title">The Golden Shoe</h1>
+    <p> The best shoe shop in the world ever!</p>
+    <Shop />
+  </div>
+  
   );
 }
 
---
+++ src/shop.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <h2>Shop Item List</h2>
+    )
+
+}
+
+export default Shop;
