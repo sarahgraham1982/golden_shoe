2b807314474a14a9343eed18cf2aa0ec73b7a3f8
sarahgraham1982 <79756187+sarahgraham1982@users.noreply.github.com>
2021-10-17 16:42:28 +0100

    added customer phone number details

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,11 +4,21 @@ const ContactForm = () => {
     return (
 
         <div className="form-wrapper">
-            <h2>Contact Us</h2>
+            <h2>Get in Touch</h2>
+            <h3>Call 0800 800 800 9am-5pm, Mon-Fri.</h3>
+            <h4>Alternatively send us a message below and we will reply within 24hours.</h4>
             <form>
             <fieldset>
                 <label>
                     <p>Name</p>
+                    <input name="name" />
+                </label>
+                <label>
+                    <p>Email</p>
+                    <input name="name" />
+                </label>
+                <label>
+                    <p>Message</p>
                     <input name="name" />
                 </label>
             </fieldset>
