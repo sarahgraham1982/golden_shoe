936238064f11bb675e0bd1ca308d16c7467cfd54
sarahgraham1982 <79756187+sarahgraham1982@users.noreply.github.com>
2021-10-17 14:44:57 +0100

    added link to announcement bar and css

--- src/components/AnnouncementBar.js
+++ src/components/AnnouncementBar.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import '../css/AnnouncementBar.css';
 
 const AnnouncementBar = () => {
     return (
         <div className="announcement-bar">
-        <h5>FREE UK DELIVERY & RETURNS!</h5>
+            <ul>
+                <li>
+                    <Link to="/CustomerService">FREE UK DELIVERY & RETURNS!</Link>
+                </li>
+            </ul>
         </div>
     )
 }
 
 
 export default AnnouncementBar;
+
