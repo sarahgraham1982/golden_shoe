c6da3110b339296f4b5ea8d4b6496ba2a7d7861d
sarahgraham1982 <79756187+sarahgraham1982@users.noreply.github.com>
2021-10-18 15:53:53 +0100

    added size chart collapsible and FAQ title

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -5,10 +5,14 @@ const Faq = () => {
     return (
         <>
             <div className="faq">
+                <h2>FAQ</h2>
                 <Collapsible trigger={<button>UK DELIVERY & RETURNS</button>}>
                     <p>blah  blah blah</p>                        
                 </Collapsible>
                 <Collapsible trigger={<button>INTERNATIONAL DELIVERY & RETURNS</button>}>
+                    <p>blah blah blah</p>                        
+                </Collapsible>
+                <Collapsible trigger={<button>SIZE CHART</button>}>
                     <p>blah blah blah</p>                        
                 </Collapsible>
             </div>
