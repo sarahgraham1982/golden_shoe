683929abfdb3ca96dac796e7049dee3a4dce8ada
sarahgraham1982 <79756187+sarahgraham1982@users.noreply.github.com>
2021-10-18 12:43:48 +0100

    added cart page

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import HomePage from './components/HomePage';
 import CustomerService from './components/CustomerService';
 import AnnouncementBar from './components/AnnouncementBar';
 import ContactForm from './components/ContactForm';
+import Cart from './components/Cart';
 
 
 function App() {
@@ -36,6 +37,7 @@ function App() {
         <Route path="/slippers" component={Slippers} />
         <Route path="/customerservice" component={CustomerService} />
         <Route path="/contactform" component={ContactForm} />
+        <Route path="/Cart" component={Cart} />
         </Switch>
       </>
     </Router>
---
+++ src/components/Cart.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <>
+            <h1>YOUR CART IS EMPTY</h1>
+            <h3>Let's go shopping!</h3>
+        </>
+        
+    )
+}
+
+
+
+
+
+export default Cart;
